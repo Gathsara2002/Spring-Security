b76d67ce5b4ccecdd9b6b785e99dc945d31c53af
Gathsara2002 <liviru2002mmg@gmail.com>
2025-03-04 16:37:31 +0530

    create security configuation class

---
+++ src/main/java/com/security/SpringSecEx/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.security.SpringSecEx.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author : Gathsara
+ * created : 3/4/2025 -- 3:53 PM
+ **/
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf(cutomizer -> cutomizer.disable());
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated());
+//        //http.formLogin(Customizer.withDefaults());
+//        http.httpBasic(Customizer.withDefaults());
+//        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//       return http.build();
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+}
